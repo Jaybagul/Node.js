e017db2b62d21213d1bf131d274c72d6b09780e4
Jayeshbagul <jbagul366@gmail.com>
2024-10-19 10:28:26 +0530

    crud

--- crud_op/server/index.js
+++ crud_op/server/index.js
@@ -6,85 +6,93 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/getdata", (req, res) => {
+const readDB = (callback) => {
   fs.readFile("./db.json", "utf-8", (err, data) => {
-    if (!err) {
-      res.send(data);
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, JSON.parse(data));
     }
   });
+};
+
+const writeDB = (data, callback) => {
+  fs.writeFile("./db.json", JSON.stringify(data), (err) => {
+    callback(err);
+  });
+};
+
+app.get("/getdata", (req, res) => {
+  readDB((err, data) => {
+    if (err) {
+      return res.status(500).send("Error reading database.");
+    }
+    res.json(data);
+  });
 });
+
 
 app.post("/adddata", (req, res) => {
-  const getDataFromFrontend = req.body;
-  fs.readFile("./db.json", "utf-8", (err, data) => {
-    if (!err) {
-      let dbData = JSON.parse(data);
-      dbData.product.push(getDataFromFrontend);
-      fs.writeFile("./db.json", JSON.stringify(dbData), (err) => {
-        if (!err) {
-          res.send("added product successfully");
-        } else {
-          res.send("something failed");
-        }
-      });
-    } else {
-      res.send("Error while reading db.json");
+  const newProduct = req.body;
+  readDB((err, dbData) => {
+    if (err) {
+      return res.status(500).send("Error reading database.");
     }
+    dbData.product.push(newProduct);
+    writeDB(dbData, (writeErr) => {
+      if (writeErr) {
+        return res.status(500).send("Error writing to database.");
+      }
+      res.send("Product added successfully");
+    });
   });
 });
+
 
 app.put("/update/:id", (req, res) => {
-  const getDataFromParam = req.params.id;
-  fs.readFile("./db.json", "utf-8", (err, data) => {
+  const productId = parseInt(req.params.id);
+  const updatedProduct = req.body;
+
+  readDB((err, dbData) => {
     if (err) {
       return res.status(500).send("Error reading database.");
     }
-    try {
-      const products = JSON.parse(data);
-      products.product.forEach((el, index) => {
-        if (el.id === parseInt(getDataFromParam)) {
-          // products.product.splice(index, 1, req.body);
-          products.product[index] = req.body;
-          fs.writeFile("./db.json", JSON.stringify(products), (writeErr) => {
-            if (writeErr) {
-              return res.status(500).send("Error writing to database.");
-            }
-            res.send("Update product successfully");
-          });
-        }
-      });
-    } catch (parseError) {
-      return res.status(500).send("Error parsing database.");
+    const productIndex = dbData.product.findIndex((p) => p.id === productId);
+    if (productIndex === -1) {
+      return res.status(404).send("Product not found.");
     }
+    dbData.product[productIndex] = updatedProduct;
+    writeDB(dbData, (writeErr) => {
+      if (writeErr) {
+        return res.status(500).send("Error writing to database.");
+      }
+      res.send("Product updated successfully");
+    });
   });
 });
 
+
 app.delete("/delete/:id", (req, res) => {
-  const getIdForDelete = req.params.id;
-  fs.readFile("./db.json", "utf-8", (err, data) => {
+  const productId = parseInt(req.params.id);
+
+  readDB((err, dbData) => {
     if (err) {
       return res.status(500).send("Error reading database.");
     }
-
-    try {
-      const products = JSON.parse(data);
-      products.product.forEach((el, index) => {
-        if (el.id === parseInt(getIdForDelete)) {
-          products.product.splice(index, 1);
-          fs.writeFile("./db.json", JSON.stringify(products), (writeErr) => {
-            if (writeErr) {
-              return res.status(500).send("Error writing to database.");
-            }
-            res.send("Delete product successfully");
-          });
-        }
-      });
-    } catch (parseError) {
-      return res.status(500).send("Error parsing database.");
+    const productIndex = dbData.product.findIndex((p) => p.id === productId);
+    if (productIndex === -1) {
+      return res.status(404).send("Product not found.");
     }
+    dbData.product.splice(productIndex, 1);
+    writeDB(dbData, (writeErr) => {
+      if (writeErr) {
+        return res.status(500).send("Error writing to database.");
+      }
+      res.send("Product deleted successfully");
+    });
   });
 });
 
 app.listen(5050, () => {
-  console.log("running on port 5050");
+  console.log("Server running on port 5050");
 });
